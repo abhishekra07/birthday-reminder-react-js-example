e8cb61f265c2df74e4b59e18cf5bb059196377d3
abhishekra07 <abhira719@gmail.com>
2023-02-06 09:43:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
 function App() {
+  const [people, setPeople] =  useState(data);
   return (
-    <h1>Birthday App</h1>
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthday's today</h3>
+        <List people={people}/> 
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
